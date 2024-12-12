2d944ad036cf4e5695872dbd3a60a0b3f2e3a646
NameFarhan <Farhanalibwn1215@gmail.com>
2024-12-12 23:00:45 +0500

    Finished Deals And Offers

--- src/Components/Deals.jsx
+++ src/Components/Deals.jsx
@@ -1,18 +1,119 @@
-
-
-import { useTheme } from '@emotion/react'
-import React from 'react'
-import { DealsWrapper } from './DealsWrapper'
+import { useTheme } from "@emotion/react";
+import React from "react";
+import { DealsWrapper } from "./DealsWrapper";
+import { Box, Grid2 } from "@mui/material";
+import Deals_Card_1 from "./Deals_Card_1";
+import Deals_Card_2 from "./Deals_Card_2";
+import Deals_Card_3 from "./Deals_Card_3";
+import Deals_Card_4 from "./Deals_Card_4";
 
 const Deals = () => {
-    const theme = useTheme()
+  const theme = useTheme();
   return (
     <>
-    <DealsWrapper>
-        Hey world
-    </DealsWrapper>
+      <DealsWrapper>
+        <Grid2
+          container
+          spacing={{
+            mobile: 12,
+            laptop: 6,
+          }}
+        >
+          <Grid2
+            item
+            size={{
+              mobile: 12,
+              tablet: 6,
+              laptop: 4,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: {
+                  mobile: "center",
+                  tablet: "center",
+                  laptop: "flex-start",
+                },
+                alignItems: "center",
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <Deals_Card_1 />
+            </Box>
+          </Grid2>
+          <Grid2
+            item
+            size={{
+              mobile: 12,
+              tablet: 6,
+              laptop: 4,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: {
+                  mobile: "center",
+                },
+                alignItems: "center",
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <Deals_Card_2 />
+            </Box>
+          </Grid2>
+          <Grid2
+            item
+            size={{
+              mobile: 12,
+              tablet: 6,
+              laptop: 4,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: {
+                  mobile: "center",
+                },
+                alignItems: "center",
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <Deals_Card_3 />
+            </Box>
+          </Grid2>
+          <Grid2
+            item
+            size={{
+              mobile: 12,
+              tablet: 6,
+              laptop: 4,
+            }}
+          >
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: {
+                  mobile: "center",
+                  laptop: "flex-start",
+                },
+                alignItems: "center",
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <Deals_Card_4 />
+            </Box>
+          </Grid2>
+        </Grid2>
+      </DealsWrapper>
     </>
-  )
-}
+  );
+};
 
-export default Deals
+export default Deals;
---
+++ src/Components/Deals_Card_3.jsx
@@ -0,0 +1,98 @@
+import { useTheme } from "@emotion/react";
+import { Box, Typography } from "@mui/material";
+import card3 from "../Images/card3.webp";
+
+const Deals_Card_3 = () => {
+  const theme = useTheme();
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: {
+            mobile: "center",
+            laptop: "flex-start",
+          },
+          alignItems: {
+            mobile: "center",
+            laptop: "flex-start",
+          },
+          textAlign: {
+            mobile: "center",
+            laptop: "start",
+          },
+          gap: "30px",
+        }}
+      >
+        <Box sx={{ position: "relative" }}>
+          <Box
+            component="img"
+            sx={{
+              height: "100%",
+              width: "100%",
+              display: "block",
+              objectFit: "cover",
+              borderRadius: "15px",
+            }}
+            alt="card1"
+            src={card3}
+          />
+          <Box
+            sx={{
+              position: "absolute",
+              bottom: "0px",
+              left: "0px",
+              backgroundColor: "#FFB30E",
+              color: "#fff",
+              padding: "5px 15px",
+              borderRadius: "0 32px 0 0",
+              fontWeight: "1000",
+              fontSize: {
+                mobile: "34px",
+                tablet: "34px",
+                laptop: "44px",
+                desktop: "54px",
+              },
+            }}
+          >
+            25% Off
+          </Box>
+        </Box>
+        <Box>
+          <Typography
+            sx={{
+              color: "#424242",
+              fontWeight: "800",
+              fontSize: {
+                mobile: "19px",
+                laptop: "22px",
+              },
+            }}
+          >
+            Greys Vage
+          </Typography>
+        </Box>
+        <Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start",
+              alignItems: "center", // Fixed typo from "alignItem" to "alignItems"
+              backgroundColor: "rgba(241, 114, 40, 0.2)", // Low opacity background
+              color: "rgba(241, 114, 40, 1)", // High opacity text color
+              fontWeight: "800",
+              borderRadius: "8px",
+              padding: "5px 10px", // Added padding for better appearance
+              width: "max-content",
+            }}
+          >
+            7 Days Remaining
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Deals_Card_3;
